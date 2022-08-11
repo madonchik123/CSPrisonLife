c310817bf2d9e0e10e142c95146cb77ab925bf79
madonchik123 <68397448+madonchik123@users.noreply.github.com>
2022-08-11 17:08:20 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,17 @@
-local Inmates = game:GetService("Teams").Inmates
-local Criminals = game:GetService("Teams").Criminals
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "CS Prison Life", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+local Tab = Window:MakeTab({
+	Name = "Main",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local Section = Tab:AddSection({
+	Name = "Autofarm"
+})
+
 local Players = game:GetService("Players")
 local Client = Players.LocalPlayer
-
      local function GetTool(Name)
       local Tool = Client.Backpack:FindFirstChild(Name)
       return Tool or Client.Character:FindFirstChild(Name)
@@ -20,13 +29,33 @@ local Client = Players.LocalPlayer
          Humanoid:EquipTool(GetTool("Fists"))
       end
    end
+   
+Tab:AddToggle({
+	Name = "Casino Autofarm!",
+	Default = false,
+	Callback = function(Value)
+getgenv().AutoFarm = Value
+if getgenv().AutoFarm then
+
+
+OrionLib:MakeNotification({
+	Name = "Info!",
+	Content = "Autofarm is now on",
+	Image = "rbxassetid://4483345998",
+	Time = 3
+})
+
+end
+
+while getgenv().AutoFarm do
 for i,v in pairs(game:GetService("Workspace").Map.Casino:GetChildren()) do
-if v.Main.Screen.Attachment:FindFirstChild("ProximityPrompt") and v.Main.LED.Color ~= Color3.new(255,0,0) then
+if v.Main.Screen.Attachment:FindFirstChild("ProximityPrompt") and getgenv().AutoFarm then
+
 TeleportChar(v.Main.Screen.CFrame  + v.Main.Screen.CFrame.LookVector *2)
 wait()
 fireproximityprompt(v.Main.Screen.Attachment:FindFirstChild("ProximityPrompt"))
 wait(1)
-if v.Main:FindFirstChild("SlotCash") then
+if v.Main:FindFirstChild("SlotCash") and getgenv().AutoFarm then
 TeleportChar(v.Main.Screen.CFrame  + v.Main.Screen.CFrame.LookVector *2)
 repeat wait() 
 for z,x in pairs(v.Main.SlotCash:GetDescendants()) do
@@ -37,8 +66,14 @@ wait(0.1)
 end
 end)
 end
-until v.Main:FindFirstChild("SlotCash") == nil
+until v.Main:FindFirstChild("SlotCash") == nil or getgenv().AutoFarm == false
 end
 
 end
 end
+
+wait()
+
+end
+	end    
+})
